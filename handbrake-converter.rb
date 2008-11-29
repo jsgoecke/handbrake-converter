853c0b25feac12c3b354e573914e3cd8b708fd09
Jason Goecke <jsgoecke@mayluc.local>
2008-11-29 11:14:43 -0800

    Fixed a bug in the DVD processing

--- handbrake-converter.rb
+++ handbrake-converter.rb
@@ -15,7 +15,7 @@ puts ''
 
 if Choice.choices[:use_dvd] == true
   puts 'Using the DVD player as the video source...'
-  src_dir_contents = [ 'DVD' ]
+  src_dir_contents = [ 'DVD.DVD' ]
 else
   puts 'Scanning source directory...'
   src_dir_contents = Dir.entries(Choice.choices[:source])
@@ -34,30 +34,34 @@ puts ''
 filetype_length = Choice.choices[:type].length
 
 src_dir_contents.each do |movie_to_convert|
-  filename_length = movie_to_convert.length
-  if filename_length > filetype_length
-    length_offset = movie_to_convert.length - filetype_length
-    if movie_to_convert.slice(length_offset, filetype_length) == Choice.choices[:type]
-      filename = movie_to_convert.split(".")
-      movie_name = filename[0]
-      if Choice.choices[:use_dvd] == true
-        conversion_instruction = @@config["handbrakecli_location"] + 
-                                 "/HandBrakeCLI -i DVD +
-                                 " -o #{Choice.choices[:destination]}/#{movie_name}" + 
-                                 @@config['handrake_presets'][Choice.choices[:conversion]]
-      else
+  if Choice.choices[:use_dvd] == true
+    conversion_instruction = @@config["handbrakecli_location"] + 
+                             "/HandBrakeCLI -i DVD" +
+                             " -o #{Choice.choices[:destination]}/#{movie_name}" + 
+                             @@config['handrake_presets'][Choice.choices[:conversion]]
+    puts 'Starting to process ' + movie_to_convert + '...'
+    puts conversion_instruction
+    system(conversion_instruction)
+    puts 'Finished processing ' + movie_to_convert + '...'
+  else
+    filename_length = movie_to_convert.length
+    if filename_length > filetype_length
+      length_offset = movie_to_convert.length - filetype_length
+      if movie_to_convert.slice(length_offset, filetype_length) == Choice.choices[:type]
+        filename = movie_to_convert.split(".")
+        movie_name = filename[0]
         conversion_instruction = @@config["handbrakecli_location"] + 
                                  "/HandBrakeCLI -i #{Choice.choices[:source]}/#{movie_to_convert}" +
                                  " -o #{Choice.choices[:destination]}/#{movie_name}" + 
                                  @@config['handrake_presets'][Choice.choices[:conversion]]
+        puts 'Starting to process ' + movie_to_convert + '...'
+        puts conversion_instruction
+        system(conversion_instruction)
       end
-      puts 'Starting to process file ' + movie_to_convert + '...'
-      puts conversion_instruction
-      system(conversion_instruction)
       if Choice.choices[:delete] == true
-        system("rm #{Choice.choices[:source]}/#{movie_to_convert}")
+          system("rm #{Choice.choices[:source]}/#{movie_to_convert}")
       end
-      puts 'Finished processing file ' + movie_to_convert + '...'
+      puts 'Finished processing ' + movie_to_convert + '...'
       puts ''
     end
   end
